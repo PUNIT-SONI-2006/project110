3cde8e1e839b7765ad8ee38370affe93aa1d2426
PUNIT-SONI-2006 <84916673+PUNIT-SONI-2006@users.noreply.github.com>
2023-11-11 15:51:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,24 @@ while response == "y":
         print("[  0  ]")
         print("[    0]")
         print("[-----]")
+    if no==4:
+        print("[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[-----]")
+    if no==5:
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+    if no==6:
+        print("[-----]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[-----]")
     
     
     response=input("Press y to roll dice and n to exit:")
-
-
